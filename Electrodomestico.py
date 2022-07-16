53dcf12136418619e5be42b36fd2bc9281b312be
luiscarlosgcd <luiscarlosgc1@hotmail.com>
2022-07-16 12:33:45 -0600

    Creacion de super clase Electrodomesticos
    incluimos getters, setters y comprobadores para validar que solo recibimos cierta informacion

---
+++ Electrodomestico.py
@@ -0,0 +1,40 @@
+class Electrodomestico:
+    def __init__(self, precio, base, color, consumo, peso):
+        self.precio = 100 #constante
+        self.color = "blanco"
+        self.consumo = 'F'
+        self.peso = 5 #constante
+
+    def setPrecio(self, precio): #getter y setter para precio
+        self.precio = precio
+    
+    def getPrecio(self):
+        return self.precio
+
+    def setColor(self, color):
+        self.color = color
+        self.comprobarColor()
+    
+    def getColor(self):
+        return self.color
+
+    def setConsumo(self, consumo):
+        self.consumo = consumo
+        self.comprobarConsumo()
+    
+    def getConsumo(self):
+        return self.consumo
+
+    def setPeso(self, peso):
+        self.peso = peso
+    
+    def getPeso(self):
+        return self.peso
+
+    def comprobarColor(self): #comprobadores para la clase para crear validaciones
+        if (self.color != 'blanco' or 'negro' or 'rojo' or 'azul' or 'gris'):
+            self.color = 'blanco'
+    
+    def comprobarConsumo(self):
+        if (self.consumo == ('A' or 'B' or 'C' or 'D' or 'E', 'F')):
+            self.consumo = 'F'
